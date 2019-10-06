965220176fee398e0106741355bfbf01f9a438c4
Chutikarn <Yuttikarn.24@gmail.com>
2019-10-06 11:28:47 +0700

    lab6

---
+++ src/lab7/ex4.java
@@ -0,0 +1,17 @@
+package lab7;
+
+public class ex4 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <10; i++) {
+            System.out.println("hello.");
+            if ((i % 2 == 0))
+                System.out.println("\tNam");
+        }
+        System.out.println("hello.");
+    }//main
+}//calss
+
+
+
+
